53c3c9ababc7eb0c41a011537125bf6c721ad40f
Mário Costa <mariocosta2760@gmail.com>
2025-03-22 05:51:06 +0100

    fix(css): força minificação no build do Tailwind

---
+++ public/assets/js/main.js
@@ -0,0 +1,60 @@
+/* ~~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~~ */
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+const closeIcon = document.getElementById("nav-close");
+
+navLink.forEach((link) => {
+    link.addEventListener("click", () => {
+        navMenu.classList.add("hidden");
+    });
+});
+
+closeIcon.addEventListener("click", () => {
+    navMenu.classList.add("hidden");
+});
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.remove("hidden");
+});
+
+/*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+//Verificando o mode da página
+if(localStorage.getItem("mode") == "dark"){
+    darkMode();
+}else{
+    lightMode();
+}
+
+themeBtn.addEventListener("click", (e) => {
+    if(localStorage.getItem("mode") == "light"){
+        darkMode();
+    }else{
+        lightMode();
+    }
+})
+
+function darkMode(){
+    html.classList.add("dark");
+    themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+    localStorage.setItem("mode", "dark");
+}
+
+function lightMode(){
+    html.classList.remove("dark");
+    themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+    localStorage.setItem("mode", "light");
+}
+
+/*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
